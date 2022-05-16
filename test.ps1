6008b1fbe9759cf9dfdf3f5c6f773f06952555af
Sergey Soroka <zverugin8@gmail.com>
2022-05-16 23:35:29 +0300

    fix some errors

--- test.ps1
+++ test.ps1
@@ -1,8 +1,9 @@
 $a=0
-get-process | ? name -match '^fire+' | ForEach-Object $_.id {
+get-process | Where-Object name -match '^fire+' | ForEach-Object $_.id {
     if ($a -le $_.id) {
      $a = $_.id 
      $b = $_.Name  
     }
 }
+$b = $b+0
 write-host $b "   " $a | Format-Table
